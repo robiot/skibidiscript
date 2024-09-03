bddec9878dc1674eadcb5849711c3193cbf623cc
Elliot <me@robiot.dev>
2024-09-03 14:50:53 +0300

    Initial parser error handling

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,68 @@
 // main.rs
+mod interpreter;
 mod lexer;
 mod parser;
-mod interpreter;
+mod error;
 
+use interpreter::Interpreter;
 use lexer::Lexer;
 use parser::Parser;
-use interpreter::Interpreter;
 use std::env;
 use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    // First check that we have a script
     if args.len() < 2 {
-        eprintln!("Usage: {} <script>", args[0]);
-        std::process::exit(1);
+        println!("Usage: {} <script>", args[0]);
+
+        return;
     }
 
+    // Read the file
     let filename = &args[1];
-    let source = fs::read_to_string(filename).expect("Could not read file");
+    let source = match fs::read_to_string(filename) {
+        Ok(source) => source,
+        Err(e) => {
+            println!("error: Couldn't read file: {}", e);
 
+            return;
+        }
+    };
+
+    // Initialize the lexer and parser
     let mut lexer = Lexer::new(&source);
-    let mut parser = Parser::new(&mut lexer);
-    let statements = parser.parse();
+
+    let mut parser = match Parser::new(&mut lexer){
+        Ok(parser) => parser,
+        Err(e) => {
+            println!("parser init error: {}", e);
+
+            return;
+        }
+    };
+
+    let statements = match parser.parse() {
+        Ok(statements) => statements,
+        Err(e) => {
+            println!("parser parse error: {}", e);
+
+            return;
+        }
+    };
 
     let mut interpreter = Interpreter::new();
     interpreter.interpret(statements);
 
+    // Run the mew function, which is the main function
     let mew_body = match interpreter.functions.get("mew") {
         Some(body) => body.clone(),
-        None => panic!("No mew function found. Stop edging and fix this problem."),
+        None => {
+            println!("error: No mew function found. Stop edging and fix this problem.");
+
+            return;
+        },
     };
     for stmt in mew_body {
         interpreter.execute_statement(stmt.clone());
--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,8 @@
 // parser.rs
-use crate::lexer::{Lexer, Token};
+use crate::{
+    error,
+    lexer::{Lexer, Token},
+};
 
 #[derive(Clone, Debug)]
 pub enum Expr {
@@ -47,38 +50,45 @@ pub struct Parser<'a> {
 }
 
 impl<'a> Parser<'a> {
-    pub fn new(lexer: &'a mut Lexer) -> Self {
-        let current_token = lexer.next_token();
-        Parser {
+    pub fn new(lexer: &'a mut Lexer) -> Result<Self, error::ParseError> {
+        let current_token = lexer.next_token()?;
+
+        Ok(Parser {
             lexer,
             current_token,
-        }
+        })
     }
 
-    fn next_token(&mut self) {
-        self.current_token = self.lexer.next_token();
+    fn next_token(&mut self) -> Result<(), error::ParseError> {
+        self.current_token = self.lexer.next_token()?;
+
+        Ok(())
     }
 
-    fn expect_token(&mut self, expected: Token) {
+    fn expect_token(&mut self, expected: Token) -> Result<(), error::ParseError> {
         if self.current_token == expected {
-            self.next_token();
+            self.next_token()?;
+
+            Ok(())
         } else {
-            panic!(
-                "Expected token {:?}, found {:?}",
-                expected, self.current_token
-            );
+            Err(error::ParseError::UnexpectedToken {
+                expected: expected.clone(),
+                found: self.current_token.clone(),
+                line: self.lexer.line,
+            })
         }
     }
 
-    pub fn parse(&mut self) -> Vec<Stmt> {
+    pub fn parse(&mut self) -> Result<Vec<Stmt>, error::ParseError> {
         let mut statements = Vec::new();
         while self.current_token != Token::EOF {
-            statements.push(self.parse_statement());
+            statements.push(self.parse_statement()?);
         }
-        statements
+
+        Ok(statements)
     }
 
-    fn parse_statement(&mut self) -> Stmt {
+    fn parse_statement(&mut self) -> Result<Stmt, error::ParseError> {
         match self.current_token {
             Token::Cookable => self.parse_function(),
             Token::Ident(ref ident) if ident == "score" => self.parse_increment(),
@@ -86,162 +96,186 @@ impl<'a> Parser<'a> {
             Token::Skibbity => self.parse_while(),
             Token::Suspect => self.parse_if(),
             Token::Blud => self.parse_return(),
-            _ => panic!("Unexpected token: {:?}", self.current_token),
+            _ => Err(
+                error::ParseError::UnknownUnexpectedToken { found: (), line: () }
+            ),
         }
     }
 
-    fn parse_function(&mut self) -> Stmt {
-        self.expect_token(Token::Cookable);
+    fn parse_function(&mut self) -> Result<Stmt, error::ParseError> {
+        self.expect_token(ßToken::Cookable)?;
         let name = if let Token::Ident(ident) = &self.current_token {
             ident.clone()
         } else {
             panic!("Expected function name");
         };
-        self.next_token();
-        self.expect_token(Token::LeftParen);
-        self.expect_token(Token::RightParen);
+        self.next_token()?;
+        self.expect_token(Token::LeftParen)?;
+        self.expect_token(Token::RightParen)?;
 
         let mut body = Vec::new();
         while self.current_token != Token::Slay && self.current_token != Token::EOF {
-            body.push(self.parse_statement());
+            body.push(self.parse_statement()?);
         }
 
-        self.expect_token(Token::Slay);
+        self.expect_token(Token::Slay)?;
 
-        Stmt::Function { name, body }
+        Ok(Stmt::Function { name, body })
     }
 
-    fn parse_increment(&mut self) -> Stmt {
+    fn parse_increment(&mut self) -> Result<Stmt, error::ParseError> {
         let name = if let Token::Ident(ident) = &self.current_token {
-            ident.clone()
+            Ok(ident.clone())
         } else {
-            panic!("Expected variable name");
-        };
-        self.next_token();
-        self.expect_token(Token::Is);
-        self.expect_token(Token::Ident("more".into()));
-        self.expect_token(Token::Ident(name.clone()));
+            Err(error::ParseError::Other("Expected variable name".into()))
+        }?;
+        self.next_token()?;
+        self.expect_token(Token::Is)?;
+        self.expect_token(Token::Ident("more".into()))?;
+        self.expect_token(Token::Ident(name.clone()))?;
 
-        Stmt::VariableAssign {
+        Ok(Stmt::VariableAssign {
             name: name.clone(),
             value: Expr::BinOp {
                 left: Box::new(Expr::Ident(name.clone())),
                 op: "+".into(),
                 right: Box::new(Expr::Number(1)),
             },
-        }
+        })
     }
 
-    fn parse_variable_assign_or_expression(&mut self) -> Stmt {
+    fn parse_variable_assign_or_expression(&mut self) -> Result<Stmt, error::ParseError> {
         let name = if let Token::Ident(ident) = &self.current_token {
-            ident.clone()
+            Ok(ident.clone())
         } else {
-            panic!("Expected identifier");
-        };
-        self.next_token();
+            Err(error::ParseError::Other("Expected identifier".into()))
+        }?;
+
+        self.next_token()?;
 
         if self.current_token == Token::Is {
-            self.next_token();
-            let value = self.parse_expression();
-            Stmt::VariableAssign { name, value }
+            self.next_token()?;
+
+            let value = self.parse_expression()?;
+
+            Ok(Stmt::VariableAssign { name, value })
         } else {
-            let expr = self.parse_expression();
-            Stmt::Expression(expr)
+            let expr = self.parse_expression()?;
+
+            Ok(Stmt::Expression(expr))
         }
     }
 
-    fn parse_expression(&mut self) -> Expr {
+    fn parse_expression(&mut self) -> Result<Expr, error::ParseError> {
         match &self.current_token {
             Token::Ident(ident) => {
                 let name = ident.clone();
-                self.next_token();
+                self.next_token()?;
+
                 if self.current_token == Token::LeftParen {
-                    self.next_token();
+                    self.next_token()?;
                     let mut args = Vec::new();
+
                     if self.current_token != Token::RightParen {
-                        args.push(self.parse_expression());
+                        args.push(self.parse_expression()?);
                         while self.current_token == Token::Comma {
-                            self.next_token();
-                            args.push(self.parse_expression());
+                            self.next_token()?;
+                            args.push(self.parse_expression()?);
                         }
                     }
-                    self.expect_token(Token::RightParen);
-                    Expr::FunctionCall { name, args }
+                    self.expect_token(Token::RightParen)?;
+
+                    Ok(Expr::FunctionCall { name, args })
                 } else {
-                    Expr::Ident(name)
+                    Ok(Expr::Ident(name))
                 }
             }
             Token::Number(num) => {
                 let value = *num;
-                self.next_token();
-                Expr::Number(value)
+                self.next_token()?;
+
+                Ok(Expr::Number(value))
             }
             Token::StringLiteral(string) => {
                 let value = string.clone();
-                self.next_token();
-                Expr::StringLiteral(value)
+                self.next_token()?;
+                Ok(Expr::StringLiteral(value))
             }
             Token::Sigma => {
-                self.next_token();
-                Expr::Boolean(true)
+                self.next_token()?;
+                Ok(Expr::Boolean(true))
             }
             Token::Ohio => {
-                self.next_token();
-                Expr::Boolean(false)
+                self.next_token()?;
+                Ok(Expr::Boolean(false))
             }
-            _ => panic!("Unexpected token: {:?}", self.current_token),
+            _ => Err(error::ParseError::UnexpectedToken {
+                expected: Token::Ident("identifier".into()),
+                found: self.current_token.clone(),
+                line: self.lexer.line,
+                message: "Unexpected token. Unknown".into(),
+            }),
         }
     }
 
-    fn parse_while(&mut self) -> Stmt {
-        self.expect_token(Token::Skibbity);
-        self.expect_token(Token::LeftParen);
-        let condition = self.parse_expression();
-        self.expect_token(Token::RightParen);
+    fn parse_while(&mut self) -> Result<Stmt, error::ParseError> {
+        self.expect_token(Token::Skibbity)?;
+        self.expect_token(Token::LeftParen)?;
+
+        let condition = self.parse_expression()?;
+
+        self.expect_token(Token::RightParen)?;
+
         let mut body = Vec::new();
+
         while self.current_token != Token::Slay && self.current_token != Token::EOF {
-            body.push(self.parse_statement());
+            body.push(self.parse_statement()?);
         }
-        self.expect_token(Token::Slay);
-        Stmt::While { condition, body }
+
+        self.expect_token(Token::Slay)?;
+
+        Ok(Stmt::While { condition, body })
     }
 
-    fn parse_if(&mut self) -> Stmt {
-        self.expect_token(Token::Suspect);
-        self.expect_token(Token::LeftParen);
-        let condition = self.parse_expression();
-        self.expect_token(Token::RightParen);
-        self.expect_token(Token::Then);
+    fn parse_if(&mut self) -> Result<Stmt, error::ParseError> {
+        self.expect_token(Token::Suspect)?;
+        self.expect_token(Token::LeftParen)?;
+        let condition = self.parse_expression()?;
+        self.expect_token(Token::RightParen)?;
+        self.expect_token(Token::Then)?;
         let mut then_branch = Vec::new();
+
         while self.current_token != Token::Ick
             && self.current_token != Token::Slay
             && self.current_token != Token::EOF
         {
-            then_branch.push(self.parse_statement());
+            then_branch.push(self.parse_statement()?);
         }
 
         let else_branch = if self.current_token == Token::Ick {
-            self.next_token();
+            self.next_token()?;
             let mut else_stmts = Vec::new();
             while self.current_token != Token::Slay && self.current_token != Token::EOF {
-                else_stmts.push(self.parse_statement());
+                else_stmts.push(self.parse_statement()?);
             }
             Some(else_stmts)
         } else {
             None
         };
 
-        self.expect_token(Token::Slay);
-        Stmt::If {
+        self.expect_token(Token::Slay)?;
+
+        Ok(Stmt::If {
             condition,
             then_branch,
             else_branch,
-        }
+        })
     }
 
-    fn parse_return(&mut self) -> Stmt {
-        self.expect_token(Token::Blud);
-        let expr = self.parse_expression();
-        Stmt::Return(expr)
+    fn parse_return(&mut self) -> Result<Stmt, error::ParseError> {
+        self.expect_token(Token::Blud)?;
+        let expr = self.parse_expression()?;
+
+        Ok(Stmt::Return(expr))
     }
 }
